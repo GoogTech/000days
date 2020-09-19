e5ffca7a77971a74b656cb2b5b72e130a6ad7821
HuangYuhui <Gentleman_0109@outlook.com>
2020-09-19 20:12:01 +0800

    update pic.py

---
+++ py/pic.py
@@ -0,0 +1,60 @@
+'''
+Author: Goog Tech
+Date: 2020-09-18 00:36:43
+LastEditTime: 2020-09-19 20:05:30
+Description: use a text of daily plans to generate a picture
+Reference: https://blog.csdn.net/www89574622/article/details/87974931
+Reference: https://selenium-python-zh.readthedocs.io/en/latest/locating-elements.html#class-name
+Reference: https://blog.csdn.net/liudinglong1989/article/details/78731754?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
+'''
+
+from selenium import webdriver
+import time
+from urllib import parse
+import os
+import shutil
+import datetime
+
+chromeFileDir = "C:\\Users\\Administrator.DESKTOP-3V51O0O\\Downloads\\"
+# thisMonthDir = os.getcwd() + "\\the template of daily plans for spe 2020\\"
+coverTemplatePath = os.getcwd() + "\\coverTemplate.md";
+coverPicDownloadPath = os.getcwd() + "\\coverPicDownload"
+
+# 打开博客封面图的模板文件
+fo = open(coverTemplatePath, "r+", encoding='UTF-8')
+# 读取文件内容
+text = fo.read()
+# 查看文件内容
+print(text)
+# 关闭连接
+fo.close()
+
+# 初始化 https://carbon.now.sh/ 网站链接
+url = "https://carbon.now.sh/?bg=rgba(126%2C211%2C33%2C1)&t=one-light" \
+      "&wt=none&l=application%2Fx-sh&ds=true&dsyoff" \
+      "=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px" \
+      "&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=4x&wm" \
+      "=false&code=" + parse.quote(text)
+
+# 反解码查看结果格式是否正确
+print(parse.unquote(url))
+
+# 点击网页中的图片下载按钮
+opt = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options = opt)
+driver.get(url)
+driver.maximize_window()
+time.sleep(1)
+driver.find_element_by_class_name('jsx-1730877631 ').click()
+print('ok: picture be downloaded successfully')
+time.sleep(30) # 图片下载到本地需要时间哟
+
+# 找到 Chrome 下载文件夹中最新下载的文件,并将其移动到当前文件夹中
+fileList=os.listdir(chromeFileDir)
+fileList.sort(key = lambda fn : os.path.getmtime(chromeFileDir + fn) if not os.path.isdir(chromeFileDir + fn) else 0)
+updateTime = datetime.datetime.fromtimestamp(os.path.getmtime(chromeFileDir + fileList[-1])) # 获取文件时间
+print('ok: new file name: ' + fileList[-1] + " and created time: " + updateTime.strftime("%Y-%m-%d %H-%M-%S"))
+
+# 移动文件
+shutil.move(chromeFileDir + fileList[-1], coverPicDownloadPath)
+print('ok: picture be moved successfully')
